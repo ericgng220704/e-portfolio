725a9e110db5b7ff26d364e0e4d97ba2a0a418b1
gnguyen2 <gnguyen2@academic.rrc.ca>
2025-05-20 11:06:18 -0500

    smooth scrolling

--- components/projects/projectSection.tsx
+++ components/projects/projectSection.tsx
@@ -14,7 +14,7 @@ design into clean, performant code.`;
 
 export default function Project() {
   return (
-    <section className="w-full mt-28 mb-8 px-4 md:px-16 lg:px-36">
+    <section id="projects" className="w-full mt-28 mb-8 px-4 md:px-16 lg:px-36">
       <div>
         <h1 className="font-normal text-4xl md:text-6xl lg:text-7xl text-blue-50 flex flex-col gap-3 mb-8">
           Featured Work
